96e6b4ceea265df21f10f409a6e9a2519acde85b
Vladimir Sh <vladirr@gmail.com>
2021-02-01 14:07:29 +0300

    feat(backend): add postgres db
    * add postgres db
    * add ormconfig.json
    * add typeorm dep

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot()
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
